ee768dfb43ad7e53517212db943c6aff4fcbb681
HiYasin <hello.mr.faisal@gmail.com>
2025-06-21 09:11:18 +0600

    feat(book): add Book interface and Mongoose model

---
+++ src/app/models/book.model.ts
@@ -0,0 +1,41 @@
+import { model, Schema } from "mongoose";
+import { IBook } from "../interfaces/book.interface";
+
+const bookSchema = new Schema<IBook>({
+    title:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    author: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    genre: {
+        type: String,
+        enum: ["FICTION", "NON_FICTION", "SCIENCE", "HISTORY", "BIOGRAPHY", "FANTASY"],
+        required: true
+    },
+    isbn: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    copies: {
+        type: Number,
+        required: true,
+        min: [0, 'Copies cannot be negative']
+    },
+    available: {
+        type: Boolean,
+        default: true
+    }
+})
+
+export const Book = model<IBook>("Book", bookSchema);
