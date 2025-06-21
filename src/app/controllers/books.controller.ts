b2d5ef6e04113d931187d30559ef788d6d15e2eb
HiYasin <hello.mr.faisal@gmail.com>
2025-06-21 10:23:48 +0600

    feat(book): add GET /api/books endpoint to fetch all books

--- src/app/controllers/books.controller.ts
+++ src/app/controllers/books.controller.ts
@@ -1,9 +1,10 @@
 import express, { Request, Response } from 'express';
 import { Book } from '../models/book.model';
+import { Error } from 'mongoose';
 
 export const bookRouter = express.Router();
 
-bookRouter.post('/',  async (req: Request, res: Response)=>{
+bookRouter.post('/', async (req: Request, res: Response) => {
     const bookData = new Book(req.body);
     try {
         const savedBook = await bookData.save();
@@ -15,8 +16,25 @@ bookRouter.post('/',  async (req: Request, res: Response)=>{
     } catch (error) {
         res.status(400).json({
             success: false,
-            message: error?.message || 'Error creating book',
+            message: 'Error creating book',
             error
         });
     }
 })
+
+bookRouter.get('/', async (req: Request, res: Response) => {
+    try {
+        const books = await Book.find();
+        res.status(200).json({
+            success: true,
+            message: 'Books retrieved successfully',
+            data: books
+        });
+    } catch(error){
+        res.status(500).json({
+            success: false,
+            message: 'Error retrieving books',
+            error
+        });
+    }
+});
