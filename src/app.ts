bc07c64ca1567377e90592f02975255a83dd3c7d
HiYasin <hello.mr.faisal@gmail.com>
2025-06-22 16:49:48 +0600

    feat: add unknown route handler middleware

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,12 @@ import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import { bookRouter } from './app/controllers/books.controller';
 import { borrowRouter } from './app/controllers/borrow.controller';
+import { unknownRouteHandler } from './app/middlewares/unknownRoute.handler';
 
 const app: Application = express();
 
 app.use(cors());
 app.use(express.json());
-
-app.use('/api/books', bookRouter);
-app.use('/api/borrow', borrowRouter);
-
-
 
 app.get('/', (req: Request, res: Response) => {
     res.send({
@@ -19,5 +15,10 @@ app.get('/', (req: Request, res: Response) => {
         message: 'Welcome to the library management app!'
     });
 });
+
+app.use('/api/books', bookRouter);
+app.use('/api/borrow', borrowRouter);
+
+app.use(unknownRouteHandler);
 
 export default app;
